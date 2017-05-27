135a673f9d2a70a60a9c6350b0810373f64f24fe
David <iceterminal@gmail.com>
2017-05-27 18:38:44 -0500

    Fixed lesson 4 js file for loop iteration

--- Web Mini-Bootcamp/LS-Web-Intro-4/script.js
+++ Web Mini-Bootcamp/LS-Web-Intro-4/script.js
@@ -13,13 +13,20 @@ document.getElementById("favorites").innerHTML = "Miami Dolphins"
 document.getElementById("hometown").innerHTML = "Indiana (shut up..I know)"
 
 //Creates a new img element and set its src attribute to a picture. Appends this element to page.
-var el = document.createElement('img');
-el.setAttribute("src", "https://avatars0.githubusercontent.com/u/17530449");
-document.body.appendChild(el);
+var image = document.createElement('img');
+image.setAttribute("src", "https://avatars0.githubusercontent.com/u/17530449");
+document.body.appendChild(image);
 
 //Iterate through each li and change the class to "listitem".
 //Adds a style tag that sets a rule for "listitem" to make the color red
-document.getElementById('nickname').className = 'listItem';
-document.getElementById('favorite').className = 'listItem';
-document.getElementById('hometown').className = 'listItem';
-document.querySelector('listItem').style.backgroundColor ='red';
+for (i=0; i<3; i++) {
+	document.getElementsByTagName('li')[i].className = 'listItem';
+	document.getElementsByTagName('li')[i].style.color = 'red';
+	document.getElementsByTagName('li')[i].style.background = 'salmon';
+}
+
+//document.getElementById('nickname').className = 'listItem';
+//document.getElementById('favorites').className = 'listItem';
+//document.getElementById('hometown').className = 'listItem';
+//document.getElementsByTagName('li')[i].style.color = 'red';
+//document.querySelector('li').style.color = 'red';  ## this way will only change the first instance of 'li'
