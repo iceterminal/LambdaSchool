a5c3a7c533e9233298926aa87d1e7c12256be97d
David <iceterminal@gmail.com>
2017-06-11 19:55:08 -0500

    Lesson 7 sucks

--- Web Mini-Bootcamp/LS-Web-Intro-7/exercises.js
+++ Web Mini-Bootcamp/LS-Web-Intro-7/exercises.js
@@ -197,56 +197,86 @@ function addProperty(object, property) {
   //add the property to the object with a value of null
   //return the object
   //note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
-  dog['favoriteColor'] = 'brown';
+  object[property] = null;
+  return object;
 }
 
 function invokeMethod(object, method) {
   //method is a string that contains the name of a method on the object
   //invoke this method
   //nothing needs to be returned
+  object[method]();
 }
 
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   //mysteryNumberObject has a property called mysteryNumber
   //multiply the mysteryNumber property by 5 and return the product
+  return mysteryNumberObject['mysteryNumber'] * 5;
 }
 
 function deleteProperty(object, property) {
   //remove the property from the object
   //return the object
+  delete object[property];
+  return object;
 }
 
 function newUser(name, email, password) {
   //create a new object with properties matching the arguments passed in.
   //return the new object
+    var user = {
+      name: name,
+      email: email,
+      password: password,
+    };
+    return user;
 }
 
 function hasEmail(user) {
   //return true if the user has a value for the property 'email'
   //otherwise return false
+  if (user['email']) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function hasProperty(object, property) {
   //return true if the object has the value of the property argument
   //property is a string
   //otherwise return false
+  if (object[property]) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function verifyPassword(user, password) {
   //check to see if the provided password matches the password property on the user object
   //return true if they match
   //otherwise return false
+  if (user[password] === 'password') {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function updatePassword(user, newPassword) {
   //replace the existing password on the user object with the value of newPassword
   //return the object
+  user['password'] = newPassword;
+  return user;
 }
 
 function addFriend(user, newFriend) {
   //user has a property called friends that is an array
   //add newFriend to the end of the friends array
   //return the user object
+  friends.push(newFriend);
+  return friends;
 }
 
 function setUsersToPremium(users) {
@@ -254,6 +284,7 @@ function setUsersToPremium(users) {
   //each user object has the property 'isPremium'
   //set each user's isPremium property to true
   //return the users array
+
 }
 
 function sumUserPostLikes(user) {
@@ -262,6 +293,7 @@ function sumUserPostLikes(user) {
   //each post object has an integer property called 'likes'
   //sum together the likes from all the post objects
   //return the sum
+
 }
 
 function addCalculateDiscountPriceMethod(storeItem) {
@@ -272,6 +304,7 @@ function addCalculateDiscountPriceMethod(storeItem) {
   //price -> 20
   //discountPercentage -> .2
   //discountPrice = 20 - (20 * .2)
+
 }
 
 
