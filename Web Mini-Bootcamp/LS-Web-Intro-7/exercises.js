7c3a30bd2ab8fcf1a883fcc30fef66af30787a26
David <iceterminal@gmail.com>
2017-06-11 19:11:40 -0500

    Lesson 7 sucks

--- Web Mini-Bootcamp/LS-Web-Intro-7/exercises.js
+++ Web Mini-Bootcamp/LS-Web-Intro-7/exercises.js
@@ -145,22 +145,38 @@ function wordsToSentence(words) {
 function contains(arr, item) {
   //check to see if item is inside of arr
   //return true if it is, otherwise return false
-  
+  return arr.indexOf(item) >=0;  
 }
 
 function addNumbers(numbers) {
   //numbers is an array of integers.
   //add all of the integers and return the value
+  var total = 0;
+  for (var i = 0; i < numbers.length; i++)
+    total += numbers[i];
+    return total;
 }
 
 function averageTestScore(testScores) {
   //testScores is an array.  Iterate over testScores and compute the average.
   //return the average
+  var avg = 0;
+  for (var i = 0; i < testScores.length; i++) {
+    avg = avg + testScores[i];
+  } avg = avg / testScores.length; {
+  return avg;
+  }
 }
 
 function largestNumber(numbers) {
   //numbers is an array of integers
   //return the largest integer
+  var large = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] > large) {
+      large = numbers[i];
+    } 
+  } return large;
 }
 
 function makeCat(name, age) {
@@ -168,12 +184,20 @@ function makeCat(name, age) {
   //add an age property to the object with the value set to the age argument
   //add a method called meow that returns the string 'Meow!'
   //return the object
+  var dog = {
+    name: name, 
+    age: age, 
+    meow: function(){
+      return 'Meow!';
+    }
+  }; return dog;
 }
 
 function addProperty(object, property) {
   //add the property to the object with a value of null
   //return the object
   //note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+  dog['favoriteColor'] = 'brown';
 }
 
 function invokeMethod(object, method) {
