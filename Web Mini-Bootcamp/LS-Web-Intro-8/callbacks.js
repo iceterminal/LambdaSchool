8f7e8969a1084c5127a781ac0f1c6440a892616a
David <iceterminal@gmail.com>
2017-06-20 21:01:39 -0500

    Lesson 8

--- Web Mini-Bootcamp/LS-Web-Intro-8/callbacks.js
+++ Web Mini-Bootcamp/LS-Web-Intro-8/callbacks.js
@@ -40,18 +40,28 @@ getLength(foods, (length) => {
 });
 
 // Write a function called last which passes the last item of the array into the callback
+function last(arr, cb){
+  cb(arr[arr.length-1]);
+}
 
 last(foods, (lastItem) => {
   console.log('The last item in the array is ' + lastItem);
 });
 
 // Write a function called sumNums that adds two numbers and passes the result to the callback
+function sumNums(x, y, cb){
+  cb(x + y);
+}
 
 sumNums(5, 10, (sum) => {
   console.log('The sum is ' + sum);
 });
 
 // Write a function called multiplyNums that adds two numbers and passes the result to the callback
+
+function multiplyNums(x, y, cb){
+  cb(x * y);
+}
 
 multiplyNums(5, 10, (product) => {
   console.log('The product is ' + product);
@@ -60,6 +70,10 @@ multiplyNums(5, 10, (product) => {
 // Write a function called contains that checks if an item is present inside of the given array.
 // Pass true to the callback if it is, otherwise pass false
 
+function contains(arr, food, cb){
+  cb(arr.includes(food));
+}
+
 contains(foods, 'ribeye', (result) => {
   console.log(result ? 'ribeye is in the array' : 'ribeye is not in the array');
 });
@@ -67,11 +81,22 @@ contains(foods, 'ribeye', (result) => {
 // Write a function called removeDuplicates that removes all duplicate values from the given array.
 // Pass the array to the callback function.  Do not mutate the original array.
 
+function removeDuplicates (arr, callback){
+	callback(arr.filter(function(elem, index, self) {
+		return index == self.indexOf(elem);
+	}));
+
 removeDuplicates(foods, (uniqueFoods) => {
   console.log('foods with duplicates removed: ' + uniqueFoods);
 });
 
 // Write a function called forEach that iterates over the provided array and passes the value and index into the callback.
+
+function forEach (arr, callback){
+	for (var i = 0; i < arr.length; i++){
+		callback(arr[i], i);
+	}
+}
 
 forEach(foods, (value, index) => {
   console.log('${value} is at index ' + index);
